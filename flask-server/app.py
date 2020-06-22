e1f60ed94487b6a0c5a0b5deba380671de6da651
mitchellpottratz <mitchell.pottratz@yahoo.com>
2020-06-22 09:44:21 -0500

    create directory for bertserver data

--- flask-server/app.py
+++ flask-server/app.py
@@ -12,10 +12,6 @@ ES_HOST = os.environ['ES_HOST']
 ES_USER = os.environ['ES_USER']
 ES_PASSWORD = os.environ['ES_PASSWORD']
 
-print(INDEX_NAME)
-print(BERT_HOST)
-print(ES_HOST)
-
 
 app = Flask(__name__)
 
